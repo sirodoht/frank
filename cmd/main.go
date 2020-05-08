7af3039d1309c41a98d09aa13267565a3eb0e01c
Theodore Keloglou <zf@sirodoht.com>
2020-05-08 14:11:07 +0100

    add cmd

---
+++ cmd/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/sirodoht/otp/totp"
+)
+
+func main() {
+	fmt.Println("Input secret:")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	if scanner.Err() != nil {
+		panic("scanner error")
+	}
+
+	secret := scanner.Text()
+
+	t := time.Now()
+	token, err := totp.GenerateCode(secret, t)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("TOTP token: %s\n", token)
+}
